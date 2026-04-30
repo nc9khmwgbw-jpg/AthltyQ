8ae3204eb6f13cc9d6db2b218a86e813d86c7c59
ayoub faham <ayoubfaham555@gmail.com>
2026-04-30 01:27:32 +0100

    clean: suppression fichiers et dossiers inutiles

--- DATA_PIPELINE/features/physique.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-def predire_charge_physique(df):
-    # Modèle d'estimation de la charge quand Opta est absent
-    return df
--- DATA_PIPELINE/features/tracking.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-def calculer_distance(df):
-    # Logique de calcul distance/sprint basée sur les stats Opta
-    return df
